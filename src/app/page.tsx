8f9bfbe2d58c0ac2b30a245120270345da10ff15
Paiss <pais.2k05@gmail.com>
2023-11-26 12:26:40 +0700

    Update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import type { Metadata } from 'next';
+
 import HomeComponent from '@/components/Home/home';
 import About from '@/components/Home/about';
 import Skill from '@/components/Home/skill';
 import Certificate from '@/components/Home/certificate';
+import Project from '@/components/Home/project';
 
 export default function Home() {
   return (
@@ -10,6 +13,24 @@ export default function Home() {
       <About />
       <Skill />
       <Certificate />
+      <Project />
     </div>
   );
 }
+
+export const metadata: Metadata = {
+  title: 'Portofolio',
+  description:
+    'Selamat datang di dunia kreatif saya! Saya Muhammad Fais Avriody Daffa, seorang mahasiswa yang memiliki minat programming.',
+  authors: [
+    { name: 'Muhammad Fais Avriody Daffa', url: 'https://allenstore.id' },
+  ],
+  category: 'Personal',
+  openGraph: {
+    title: 'Portofolio',
+    description:
+      'Selamat datang di dunia kreatif saya! Saya Muhammad Fais Avriody Daffa, seorang mahasiswa yang memiliki minat programming.',
+    url: 'https://allenstore.id',
+    type: 'website',
+  },
+};
