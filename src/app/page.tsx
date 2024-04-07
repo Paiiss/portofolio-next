bfb9c081725061ed4279f2036851141f71311569
Paiss <pais.2k05@gmail.com>
2024-04-08 05:54:03 +0700

    Update metadata URL in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Project from '@/components/Home/project';
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 pt-36">
       <HomeComponent />
       <About />
       <Skill />
@@ -22,15 +22,13 @@ export const metadata: Metadata = {
   title: 'Portofolio',
   description:
     'Selamat datang di dunia kreatif saya! Saya Muhammad Fais Avriody Daffa, seorang mahasiswa yang memiliki minat programming.',
-  authors: [
-    { name: 'Muhammad Fais Avriody Daffa', url: 'https://allenstore.id' },
-  ],
+  authors: [{ name: 'Muhammad Fais Avriody Daffa', url: 'https://peix.my.id' }],
   category: 'Personal',
   openGraph: {
     title: 'Portofolio',
     description:
       'Selamat datang di dunia kreatif saya! Saya Muhammad Fais Avriody Daffa, seorang mahasiswa yang memiliki minat programming.',
-    url: 'https://allenstore.id',
+    url: 'https://peix.my.id',
     type: 'website',
   },
 };
